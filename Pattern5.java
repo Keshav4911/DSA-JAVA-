b11a0cc9a9c921c3676ac2d0f73e720a0862bee2
Keshav Sharma <61627019+Keshav4911@users.noreply.github.com>
2024-05-21 12:33:59 +0530

    Add files via upload
    1 
    2 3 
    4 5 6 
    7 8 9 10 
    11 12 13 14 15 
    Here is the code for the above pattern.

---
+++ Pattern5.java
@@ -0,0 +1,12 @@
+public class Pattern5 {
+    public static void main(String args[]){
+        int c=1;
+        for( int i=0;i<5;i++){
+            for( int j=0;j<=i;j++){
+                System.out.print(c+ " ");
+                c++;
+            }
+            System.out.println();
+        }
+    }
+}
